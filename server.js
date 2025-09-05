d3665fcc93fc149d4404089f0be4c5300a323872
Raja7765 <rajasarvesh71@gmail.com>
2025-09-05 10:41:10 +0530

    changes done

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
-jfnfujnuneunn
+const express = require('express');
+const dotenv = require('dotenv');
+const {Pool} = require('pg');
+
+
+//Load dotenv file 
+dotenv.config();
+const app = express();
+
+//middleware
+app.use(express.json());
+
+//Postgres Pool
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+
+//test db connection
+pool.connect()
+.then(() => console.log('Database connected successfully'))
+.catch(err => console.error('Database connection error', err.stack));
+
+
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('Taxi Service is running');
+});
+
+
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}); 
+
+
+module.exports = app;
