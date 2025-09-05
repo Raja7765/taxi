5daceefad1093922ded0e244a9019f8697b6464b
Raja7765 <rajasarvesh71@gmail.com>
2025-09-05 12:49:22 +0530

    added the signup and login

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { Pool } = require("pg");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
 
 // Load environment variables
 dotenv.config();
 const app = express();
-
 
 
 // Middleware
@@ -38,8 +39,53 @@ pool.connect()
 
 // Example route
 app.get("/", (req, res) => {
-  res.send("🚖 Taxi Booking Backend is running...");
+  res.send("Taxi Booking Backend is running...");
 })
+
+// Signup
+app.post("/signup", async (req, res) => {
+  const { name, email, password, role, phone } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const result = await pool.query(
+      "INSERT INTO users (name,email,password,role,phone) VALUES($1,$2,$3,$4,$5) RETURNING *",
+      [name, email, hashedPassword, role, phone]
+    );
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: result.rows[0],
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Login
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+    const user = result.rows[0]; 
+
+    if (!user) return res.status(400).json({ error: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ error: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" } 
+    );
+
+    res.json({ message: "Login successful", token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
 
 
 
@@ -47,7 +93,7 @@ app.get("/", (req, res) => {
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log( `Server running on http://localhost:${PORT}`);
+  console.log( `Server running on ${PORT}`);
 });
 
 
