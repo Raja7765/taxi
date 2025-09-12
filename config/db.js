40320cd4316451e71504303ecc49181fb71d92f8
Raja7765 <rajasarvesh71@gmail.com>
2025-09-12 16:32:33 +0530

    adds the both working

--- config/db.js
+++ config/db.js
@@ -1,9 +1,12 @@
 const { Pool } = require("pg");
 require("dotenv").config();
+const isProduction = process.env.NODE_ENV === "production";
+
+
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {rejectUnauthorized: false }
+  ...(isProduction ? {ssl:{ rejectUnauthorized: false}} :{})
 });
 
 pool.connect()
