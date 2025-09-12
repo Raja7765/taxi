30c08c4ec0c6c24307597b2710e2939c5be5ad5b
Raja7765 <rajasarvesh71@gmail.com>
2025-09-12 17:13:18 +0530

    added the file

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || process.env.DATABASE,
   ...(isProduction ? {ssl:{ rejectUnauthorized: false}} :{})
 });
 
