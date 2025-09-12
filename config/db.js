9cf8e3768679c818297feb862786ce02be970e94
Raja7765 <rajasarvesh71@gmail.com>
2025-09-12 17:26:10 +0530

    changed the db name

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || process.env.DATABASE,
+  connectionString: process.env.DATABASE_URL,
   ...(isProduction ? {ssl:{ rejectUnauthorized: false}} :{})
 });
 
