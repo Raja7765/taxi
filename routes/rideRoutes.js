ed01c449735dd690833646874ab48ea33de4d5e1
Raja7765 <rajasarvesh71@gmail.com>
2025-09-12 14:10:33 +0530

    file name changed

--- routes/rideRoutes.js
+++ routes/rideRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { bookRide } = require("../controllers/rideController");
-const { authMiddleware } = require("../middleware/authMiddleware")
+const { authMiddleware } = require("../middleware/authMiddleware");
 const { getMyRides } = require("../controllers/rideController");
 
 router.post("/book-ride", authMiddleware, bookRide);
